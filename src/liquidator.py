eb43397f3d3ed70dde90ee9346e8de188a1fbe9e
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2020-08-05 11:40:34 +0000

    initial version

--- src/liquidator.py
+++ src/liquidator.py
@@ -1,15 +1,13 @@
 from web3 import Web3
 import os
+import json
 from dotenv import load_dotenv
-# OR, explicitly providing path to '.env'
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env.local'
-load_dotenv(dotenv_path=env_path, verbose=True)
+import requests
 
+import urllib.request
+from urllib.request import build_opener
+from http.cookiejar import CookieJar
 
-w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
-account = os.getenv('MY_WALLET_ADDRESS')
-pk = os.getenv('MY_WALLET_PK')
+from termcolor import colored
 
-
-
+# Initial version
